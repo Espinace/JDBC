b1961eaef07eb8ab475f46181ae50c6a27696ccf
Bruno Amazonas Espinace <41213376+Espinace@users.noreply.github.com>
2021-09-29 18:58:06 -0300

    Add files via upload

---
+++ jdbc/TesteConexao.java
@@ -0,0 +1,24 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteConexao {
+
+	public static void main(String[] args) throws SQLException{
+		
+		final String url = "jdbc:mysql://localhost?verifyServerCertificate=false&useSSL=true"
+				+ "&useTimezone=true&serverTimezone=UTC";
+		final String usuario = "root";
+		final String senha = "abc12345";
+		
+		Connection conexao = DriverManager.
+				getConnection(url, usuario, senha);
+		
+		System.out.println("Conexao efetuada com sucesso!");
+		conexao.close();
+				
+		
+	}
+}
